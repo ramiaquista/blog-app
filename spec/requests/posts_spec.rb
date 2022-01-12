ff94496072bdf0871eec2817d62055cb998c3b95
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-01-12 12:59:55 -0300

    Add posts controller tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe 'GET index' do
+    before(:example) { get '/users/:user_id/posts' }
+    it 'is a succes' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should render posts#index template' do
+      expect(response).to render_template('index')
+    end
+  end
+  describe 'GET show' do
+    before(:example) { get '/users/:user_id/posts/:id' }
+    it 'is a succes' do
+      expect(response).to have_http_status(:ok)
+    end
+    it 'should render posts#show template' do
+      expect(response).to render_template('show')
+    end
+  end
+end
+
+RSpec.describe 'Posts', type: :feature do
+  it 'renders the correct text in the index template' do
+    visit '/users/1/posts'
+    expect(current_path).to eq('/users/1/posts')
+    expect(page).to have_text('This shows all the posts from the user')
+  end
+  it 'renders the correct text in the show template' do
+    visit '/users/1/posts/show'
+    expect(current_path).to eq('/users/1/posts/show')
+    expect(page).to have_text('This shows full description of the selected post')
   end
 end
