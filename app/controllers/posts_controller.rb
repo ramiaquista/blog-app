7c6143fd0b9447a3e5b73e38e5346bc7ef271f8f
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-01-19 12:13:05 -0300

    Add forms to create comments and posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,12 @@ class PostsController < ApplicationController
   def index; end
 
   def show; end
+
+  def new
+    post = Post.new
+    @current_user = current_user
+    respond_to do |format|
+      format.html { render :new, locals: { post: post } }
+    end
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  has_many :comments, :likes
+  has_many :comments
+  has_many :likes
   belongs_to :author, class_name: 'User'
 
   def update_post_counter
