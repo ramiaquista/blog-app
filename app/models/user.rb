c4f87fbca1b7ac906cca2b5335b12e5b17534b0a
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-01-20 12:39:00 -0300

    Add unit specs for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: 'author_id'
   has_many :likes, foreign_key: 'author_id'
 
-  def most_recent_posts(user)
-    user.includes(:posts).limit(3).order(created_at: :desc)
+  def most_recent_posts
+    User.includes(:posts).limit(3).order(created_at: :desc)
   end
 end
---
+++ spec/model/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  subject { described_class.new }
+  describe 'Validations' do
+    it 'is valid with valid attributes' do
+      subject.name = 'Rami'
+      subject.posts_counter = 2
+      expect(subject).to be_valid
+    end
+    it 'is not valid without attributes' do
+      expect(User.new).to_not be_valid
+    end
+    it 'is not valid without a name' do
+      user = User.new(name: nil)
+      expect(user).to_not be_valid
+    end
+    it 'is not valid if posts counter is less than 0' do
+      user = User.new(name: 'Rami', posts_counter: -1)
+      expect(user).to_not be_valid
+    end
+  end
+  describe "Associations" do
+    it { should respond_to(:posts) }
+    it { should respond_to(:comments) }
+    it { should respond_to(:likes) }
+  end
+  describe "Methods" do
+    it 'should return an empty array' do
+        user = User.new(name: "Rami", posts_counter: 0)
+        expect(user.most_recent_posts).to eq([])
+    end
+  end
+end
