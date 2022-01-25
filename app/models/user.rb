3fbc39df16e680842193a02644082d7880d3e368
Ramiro Aquistapace <ramiaquista70@gmail.com>
2022-01-25 17:23:21 -0300

    Install CanCan, and generate roles column in User table with migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,13 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :posts_counter, presence: false, numericality: { greater_than_or_equal_to: 0 }
 
+  ROLES = %i[admin default].freeze
+
   def recent_posts
     Post.includes(:user).where("user_id = #{id}").references(:user).limit(3)
+  end
+
+  def is?(requested_role)
+    role == requested_role.to_s
   end
 end
