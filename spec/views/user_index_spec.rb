f6e7cfb7f2d33f5524d929a1c18152830956449b
enmanuel <enmanuellassis@gmail.com>
2022-01-26 21:08:15 -0400

    Add unit-testing to user-index

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -7,22 +7,33 @@ RSpec.describe 'User Index validation', type: :system do
         User.create(name: 'mat',
                     photo: 'https://cdnwp-s3.benzinga.com/wp-content/uploads/2021/11/18141437/CurryApe.png',
                     bio: 'po bio', posts_counter: 0, email: 'po@gmail.com', password: '123456')
-        sleep(1)
+       
         page.fill_in 'Username', with: 'po@gmail.com'
-        sleep(1)
+      
         page.fill_in 'Password', with: '123456'
-        sleep(1)
+      
         expect(page).to have_field('Username', with: 'po@gmail.com')
         expect(page).to have_field('Password', with: '123456')
         click_button('Log in')
     }
     it 'shows the number of posts of the users' do
-    sleep(5)
+   
         expect(page).to have_content("Number Of Post:")
     end
     it 'shows the username of the users' do
-        sleep(5)
-        expect(page).to have_selector("name")
+      
+        expect(page).to have_css(".name")
+    end
+    it 'shows the profile picture of user' do
+     
+        expect(page).to have_css(".user-img")
+    end
+    it 'redirects to the user profile' do
+        user_container = find(".user-container")
+        sleep(2)
+        user_container.click
+        sleep(2)
+        expect(page).to have_content("bio")
     end
   end
 end
