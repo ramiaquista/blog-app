8c99cbd1b9b3d8af969496e128d8ce678db315b3
enmanuel <enmanuellassis@gmail.com>
2022-01-27 10:38:49 -0400

    save local changes

--- spec/views/user_index_spec.rb
+++ spec/views/user_index_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe 'User Index validation', type: :system do
     describe 'User index' do
     before {
-        visit new_user_session_path
         User.create(name: 'mat',
-                    photo: 'https://cdnwp-s3.benzinga.com/wp-content/uploads/2021/11/18141437/CurryApe.png',
-                    bio: 'po bio', posts_counter: 0, email: 'po@gmail.com', password: '123456')
-       
+        photo: 'https://cdnwp-s3.benzinga.com/wp-content/uploads/2021/11/18141437/CurryApe.png',
+        bio: 'po bio', posts_counter: 0, email: 'po@gmail.com', password: '123456')
+        
+        visit new_user_session_path
         page.fill_in 'Username', with: 'po@gmail.com'
         page.fill_in 'Password', with: '123456'
         click_button('Log in')
